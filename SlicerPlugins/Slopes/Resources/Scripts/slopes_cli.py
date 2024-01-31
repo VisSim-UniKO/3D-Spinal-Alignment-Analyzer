eec5ad73b03dccf25805f40d82365cf7d778945b
Vinzent Rittel <vinzentrittel@uni-koblenz.de>
2024-01-31 14:00:46 +0100

    add output options for vertebra local axes

--- SlicerPlugins/Slopes/Resources/Scripts/slopes_cli.py
+++ SlicerPlugins/Slopes/Resources/Scripts/slopes_cli.py
@@ -1,9 +1,22 @@
 from argparse import ArgumentParser, FileType
+from json import dumps
+from sys import exit
 
-from numpy import array
+from numpy import array, inf, ndarray, set_printoptions
 
 from morphology import Spine
 from vtk_convenience import load_stl
+
+def extract_axis(spine: Spine, index: int) -> ndarray:
+    vertebra = spine[index]
+    orientation = vertebra.orientation
+    return array([orientation.left, orientation.back, orientation.up])
+
+def extract_axes(spine: Spine) -> ndarray:
+    return array([
+        extract_axis(spine, i)
+        for i, _ in enumerate(spine)
+    ])
 
 if __name__ == '__main__':
     Parser = ArgumentParser(
@@ -40,6 +53,19 @@ if __name__ == '__main__':
         default=45.0,
         help="Maximum angle a face's normal can diverge from the general up direction to be considered part of the superior endplate. (default: 45)",
     )
+    Parser.add_argument(
+        '-p',
+        '--output-local-axes',
+        action='store_true',
+        help='Print all set of local axes for each individual vertebra',
+    )
+    Parser.add_argument(
+        '-o',
+        '--output-axis',
+        metavar='INDEX',
+        type=int,
+        help='Output a specific local set of local axes. The target vertebra is indicated by the zero-based value, passed by INDEX.',
+    )
 
     Arguments = Parser.parse_args()
     Vertebrae = [load_stl(file) for file in Arguments.filenames]
@@ -49,4 +75,13 @@ if __name__ == '__main__':
         slice_thickness=Arguments.thickness,
         max_angle=Arguments.max_angle,
     )
+    if not Arguments.output_axis is None:
+        print(dumps(extract_axis(SpineRepr, Arguments.output_axis).tolist()))
+        exit()
+    if Arguments.output_local_axes:
+        set_printoptions(threshold=inf)
+        print(dumps(extract_axes(SpineRepr).tolist()))
+        exit()
+
     print(*SpineRepr.angles, sep=", ")
+
